db68bac652bc1f8fa08d08dd6267738ac1754d42
0896DanielH <0896danielh@gmail.com>
2016-10-01 21:13:07 +0200

    Search Code
    Search code for WIL

---
+++ frmSearch/frmSearch/frmSearch.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace frmSearch
+{
+    partial class frmSearch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dgvDisplay = new System.Windows.Forms.DataGridView();
+            this.cboSearchType = new System.Windows.Forms.ComboBox();
+            this.txtEnterHere = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDisplay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(371, 175);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(93, 40);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // dgvDisplay
+            // 
+            this.dgvDisplay.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDisplay.Location = new System.Drawing.Point(45, 12);
+            this.dgvDisplay.Name = "dgvDisplay";
+            this.dgvDisplay.RowTemplate.Height = 28;
+            this.dgvDisplay.Size = new System.Drawing.Size(240, 150);
+            this.dgvDisplay.TabIndex = 1;
+            // 
+            // cboSearchType
+            // 
+            this.cboSearchType.FormattingEnabled = true;
+            this.cboSearchType.Items.AddRange(new object[] {
+            "Surname",
+            "Contact Number"});
+            this.cboSearchType.Location = new System.Drawing.Point(371, 28);
+            this.cboSearchType.Name = "cboSearchType";
+            this.cboSearchType.Size = new System.Drawing.Size(121, 28);
+            this.cboSearchType.TabIndex = 2;
+            // 
+            // txtEnterHere
+            // 
+            this.txtEnterHere.Location = new System.Drawing.Point(371, 96);
+            this.txtEnterHere.Name = "txtEnterHere";
+            this.txtEnterHere.Size = new System.Drawing.Size(100, 26);
+            this.txtEnterHere.TabIndex = 3;
+            // 
+            // frmSearch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 228);
+            this.Controls.Add(this.txtEnterHere);
+            this.Controls.Add(this.cboSearchType);
+            this.Controls.Add(this.dgvDisplay);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "frmSearch";
+            this.Text = "Search";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDisplay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView dgvDisplay;
+        private System.Windows.Forms.ComboBox cboSearchType;
+        private System.Windows.Forms.TextBox txtEnterHere;
+    }
+}
+
---
+++ frmSearch/frmSearch/frmSearch.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace frmSearch
+{
+    public partial class frmSearch : Form
+    {
+        public frmSearch()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Declaration
+                string searchType = cboSearchType.Text;
+
+                if (searchType.Equals("Surname"))
+                {
+                    string enter = txtEnterHere.Text;
+                    DBConnect dObj = new DBConnect();
+                    dObj.OpenConnection();
+                    
+
+                    dObj.sqlCmd = new SqlCommand("SELECT * FROM PATIENT WHERE PAT_SURNAME=@Search", dObj.sqlCon);
+                    dObj.sqlCmd.Parameters.AddWithValue("@Search", enter);
+
+                    int rows = dObj.sqlCmd.ExecuteNonQuery();
+                    dObj.sqlCmd.Connection = dObj.sqlCon;
+
+                    dObj.sqlDR = dObj.sqlCmd.ExecuteReader();
+                    while (dObj.sqlDR.Read())
+                    {
+                        dgvDisplay.Rows.Add(new object[] { "PATIENT_ID", dObj.sqlDR["PAT_ID"].ToString() });
+                        dgvDisplay.Rows.Add(new object[] { "PATIENT_NAME", dObj.sqlDR["PAT_NAME"].ToString() });
+                        dgvDisplay.Rows.Add(new object[] { "PATIENT_SURNAME", dObj.sqlDR["PAT_SURNAME"].ToString() });
+                        dgvDisplay.Rows.Add(new object[] { "PATIENT_CONTACT_NO", dObj.sqlDR["PAT_NO"].ToString() });
+                        dgvDisplay.Rows.Add(new object[] { "APPOINTMENT_DATE", dObj.sqlDR["APPOINT_DATE"].ToString() });
+                        dgvDisplay.Rows.Add(new object[] { "APPOINTMENT_TIMES", dObj.sqlDR["APPOINT_TIMES"].ToString() });
+                    }
+                    dObj.sqlDR.Close();
+                    dObj.sqlCon.Close();
+                }
+
+                else if (searchType.Equals("Contact Number"))
+                {
+                    string enter = txtEnterHere.Text;
+                    DBConnect dObj = new DBConnect();
+                    dObj.OpenConnection();
+
+                    dObj.sqlCmd = new SqlCommand("SELECT * FROM PATIENT WHERE PAT_NO=@Search", dObj.sqlCon);
+                    dObj.sqlCmd.Parameters.AddWithValue("@Search", enter);
+
+                    int rows = dObj.sqlCmd.ExecuteNonQuery();
+                    dObj.sqlCmd.Connection = dObj.sqlCon;
+
+                    dObj.sqlDR = dObj.sqlCmd.ExecuteReader();
+                    while (dObj.sqlDR.Read())
+                    {
+                        dgvDisplay.Rows.Add(new object[] { "PATIENT_ID", dObj.sqlDR["PAT_ID"].ToString() });
+                        dgvDisplay.Rows.Add(new object[] { "PATIENT_NAME", dObj.sqlDR["PAT_NAME"].ToString() });
+                        dgvDisplay.Rows.Add(new object[] { "PATIENT_SURNAME", dObj.sqlDR["PAT_SURNAME"].ToString() });
+                        dgvDisplay.Rows.Add(new object[] { "PATIENT_CONTACT_NO", dObj.sqlDR["PAT_NO"].ToString() });
+                        dgvDisplay.Rows.Add(new object[] { "APPOINTMENT_DATE", dObj.sqlDR["APPOINT_DATE"].ToString() });
+                        dgvDisplay.Rows.Add(new object[] { "APPOINTMENT_TIME", dObj.sqlDR["APPOINT_TIMES"].ToString() });
+                    }
+                    dObj.sqlDR.Close();
+                    dObj.sqlCon.Close();
+                }
+            }
+            catch (System.ArgumentException ex1)
+            {
+                MessageBox.Show(ex1.Message);
+            }
+            catch (System.ObjectDisposedException ex2)
+            {
+                MessageBox.Show(ex2.Message);
+            }
+            catch (System.FormatException ex3)
+            {
+                MessageBox.Show(ex3.Message);
+            }
+            catch (System.IndexOutOfRangeException ex4)
+            {
+                MessageBox.Show(ex4.Message);
+            }
+            catch (SqlException ex5)
+            {
+                MessageBox.Show(ex5.Message);
+            } 
+        }
+    }
+}

77a2c1a8176e72602a40d86fbb6123ccbc47f8c2
0896DanielH <0896danielh@gmail.com>
2016-10-01 21:08:01 +0200

    Add Code
    Add code for WIL

---
+++ frmAdd/frmAdd/frmAdd.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace frmAdd
+{
+    partial class frmAdd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAddID = new System.Windows.Forms.TextBox();
+            this.txtAddName = new System.Windows.Forms.TextBox();
+            this.txtAddSurname = new System.Windows.Forms.TextBox();
+            this.txtAddContactNum = new System.Windows.Forms.TextBox();
+            this.txtAddAppointTime = new System.Windows.Forms.TextBox();
+            this.dtpAddAppointDate = new System.Windows.Forms.DateTimePicker();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtAddID
+            // 
+            this.txtAddID.Location = new System.Drawing.Point(12, 21);
+            this.txtAddID.Name = "txtAddID";
+            this.txtAddID.Size = new System.Drawing.Size(100, 26);
+            this.txtAddID.TabIndex = 0;
+            // 
+            // txtAddName
+            // 
+            this.txtAddName.Location = new System.Drawing.Point(152, 21);
+            this.txtAddName.Name = "txtAddName";
+            this.txtAddName.Size = new System.Drawing.Size(100, 26);
+            this.txtAddName.TabIndex = 1;
+            // 
+            // txtAddSurname
+            // 
+            this.txtAddSurname.Location = new System.Drawing.Point(285, 21);
+            this.txtAddSurname.Name = "txtAddSurname";
+            this.txtAddSurname.Size = new System.Drawing.Size(100, 26);
+            this.txtAddSurname.TabIndex = 2;
+            // 
+            // txtAddContactNum
+            // 
+            this.txtAddContactNum.Location = new System.Drawing.Point(12, 86);
+            this.txtAddContactNum.Name = "txtAddContactNum";
+            this.txtAddContactNum.Size = new System.Drawing.Size(100, 26);
+            this.txtAddContactNum.TabIndex = 3;
+            // 
+            // txtAddAppointTime
+            // 
+            this.txtAddAppointTime.Location = new System.Drawing.Point(152, 86);
+            this.txtAddAppointTime.Name = "txtAddAppointTime";
+            this.txtAddAppointTime.Size = new System.Drawing.Size(101, 26);
+            this.txtAddAppointTime.TabIndex = 4;
+            // 
+            // dtpAddAppointDate
+            // 
+            this.dtpAddAppointDate.Location = new System.Drawing.Point(12, 146);
+            this.dtpAddAppointDate.Name = "dtpAddAppointDate";
+            this.dtpAddAppointDate.Size = new System.Drawing.Size(200, 26);
+            this.dtpAddAppointDate.TabIndex = 5;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(280, 146);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(105, 39);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // frmAdd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 221);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dtpAddAppointDate);
+            this.Controls.Add(this.txtAddAppointTime);
+            this.Controls.Add(this.txtAddContactNum);
+            this.Controls.Add(this.txtAddSurname);
+            this.Controls.Add(this.txtAddName);
+            this.Controls.Add(this.txtAddID);
+            this.Name = "frmAdd";
+            this.Text = "Add";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAddID;
+        private System.Windows.Forms.TextBox txtAddName;
+        private System.Windows.Forms.TextBox txtAddSurname;
+        private System.Windows.Forms.TextBox txtAddContactNum;
+        private System.Windows.Forms.TextBox txtAddAppointTime;
+        private System.Windows.Forms.DateTimePicker dtpAddAppointDate;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
+
---
+++ frmAdd/frmAdd/frmAdd.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace frmAdd
+{
+    public partial class frmAdd : Form
+    {
+        public frmAdd()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            //Declarations
+            string patientID = txtAddID.Text;
+            string patientName = txtAddName.Text;
+            string patientSurname = txtAddSurname.Text;
+            string patientNumber = txtAddContactNum.Text;
+            string appointmentDate = dtpAddAppointDate.Text;
+            string appointmentTime = txtAddAppointTime.Text;
+
+            try
+            {
+                DBConnect db = new DBConnect();
+                db.OpenConnection();
+                SqlCommand sqlCmd = new SqlCommand();
+                sqlCmd.Parameters.AddWithValue("@patientID", txtAddID.Text);
+                sqlCmd.Parameters.AddWithValue("@patientName", txtAddName.Text);
+                sqlCmd.Parameters.AddWithValue("@patientSurname", txtAddSurname.Text);
+                sqlCmd.Parameters.AddWithValue("@patientNumber", txtAddContactNum.Text);
+                sqlCmd.Parameters.AddWithValue("@appointmentDate", dtpAddAppointDate.Text);
+                sqlCmd.Parameters.AddWithValue("@appointmentTime", txtAddAppointTime.Text);
+                db.SetSqlCommand(sqlCmd);
+                string insertStatement = "INSERT INTO Patient VALUES (@patientID, @patientName, @patientSurname, @patientNumber, @appointmentDate, @appointmentTime)";
+                db.GiveCommand(insertStatement);
+                MessageBox.Show("Patient data successfully added!");
+
+                db.CloseConnection();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
